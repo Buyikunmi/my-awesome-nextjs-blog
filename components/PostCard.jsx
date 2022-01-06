5031b4e7cb9c84ca5229378a8b760ed67592156a
Agbaeze Emmanuel <agbaezeemmanuel@gmail.com>
2022-01-06 06:05:54 +0100

    added sidebar widgets and update the postCard component

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,10 +1,48 @@
 import React from "react";
+import moment from "moment";
+
+import Link from "next/link";
 
 const PostCard = ({ post }) => {
   return (
-    <div>
-      {post.title}
-      {post.excerpt}
+    <div className="bg-white shadow-lg rounded-lg p-4 lg:p-8 pb-12 mb-8">
+      <div className="relative overflow-hidden shadow-md  mb-6">
+        <img
+          src={post.featuredImage.url}
+          alt={post.title}
+          className="w-full h-80 object-cover object-top shadow-lg rounded-t-lg lg:rounded-lg"
+        />
+      </div>
+      <h1 className="transition duration-700 text-center mb-8 cursor-pointer hover:text-pink-600 font-semibold">
+        <Link href={`/posts/${post.slug}`}>{post.title}</Link>
+      </h1>
+      <div className="block lg:flex text-center items-center justify-center mb-8 w-full">
+        <div className="flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8">
+          <img
+            src={post.author.photo.url}
+            alt={post.author.name}
+            className="align-middle roudned-full"
+            height="30px"
+            width="30px"
+          />
+          <p className="inline align-middle text-gray-700 ml-2 text-lg">
+            {post.author.name}
+          </p>
+        </div>
+        <div className="font-mediun text-gray-700">
+          <span>{moment(post.createdAt).format("MMM DD, YYYY")}</span>
+        </div>
+      </div>
+      <p className="text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8">
+        {post.excerpt}
+      </p>
+      <div className="text-center">
+        <Link href={`/post/${post.slug}`}>
+          <span className="transition duration-500 transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">
+            Continue Reading
+          </span>
+        </Link>
+      </div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,27 @@
 import Head from "next/head";
 import { PostCard, Categories, PostWidget } from "../components";
-const posts = [
-  { title: "React Testing with Jest", excerpt: "React Testing with Jest" },
-  { title: "React with Typescript", excerpt: "React with typescript" },
-];
-export default function Home() {
+import { getPosts } from "../services";
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  console.log(`post is`, posts);
+  return {
+    props: { posts },
+  };
+}
+export default function Home({ posts }) {
   return (
-    <div className="container mx-auto px-10 mb-8">
+    <div className="container mx-auto px-4 lg:px-10 mb-8 ">
       <Head>
         <title>My Blog CMS</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        <div className="lg:col-span-8 col-span-1">
+        <div className="lg:col-span-8 col-span-1 ">
           {posts.map((post, index) => (
-            <PostCard key={index} post={post} />
+            <PostCard key={index} post={post.node} />
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
